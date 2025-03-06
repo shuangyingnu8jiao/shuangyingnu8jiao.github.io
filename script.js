233e68e289e69ad4769beb34e74527459be46d4b
shuangyingnu8jiao <2826123375@qq.com>
2025-03-06 23:17:51 +0800

    Update script.js

--- script.js
+++ script.js
@@ -49,10 +49,10 @@ noButton.addEventListener("click", function () {
   }
 
   // 图片变化（前 5 次变化）
-  if (clickCount === 1) mainImage.src = "images/shocked.ipg"; // 震惊
-  if (clickCount === 2) mainImage.src = "images/think.ipg"; // 思考
+  if (clickCount === 1) mainImage.src = "images/shocked.jpg"; // 震惊
+  if (clickCount === 2) mainImage.src = "images/think.jpg"; // 思考
   if (clickCount === 3) mainImage.src = "images/angry.jpg"; // 生气
-  if (clickCount === 4) mainImage.src = "images/crying.ipg"; // 哭
+  if (clickCount === 4) mainImage.src = "images/crying.jpg"; // 哭
   if (clickCount >= 5) mainImage.src = "images/crying.jpg"; // 之后一直是哭
 });
 
